79801654d0b8611f466d9555808d1a86139506f2
Malek <mohamedmalekalfakih@istic.ucar.tn>
2025-04-01 11:43:37 +0200

    readthedocs htmlk

--- conf.py
+++ conf.py
@@ -25,3 +25,9 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('.'))
+
+html_output = os.getenv('READTHEDOCS_OUTPUT', '_build/html')
